20e44741e5e265dd23c812fd3348dc4ab61b463b
msrosenberg <drmsrosenberg@gmail.com>
2020-06-19 14:41:18 -0400

    cleaning up some test inconsistencies

--- tests/test_distcon.py
+++ tests/test_distcon.py
@@ -1,5 +1,6 @@
 import numpy
 import pytest
+import math
 import pyssage.distcon
 import pyssage.graph
 from tests.test_common import test_coords, load_answer
@@ -7,7 +8,7 @@ from tests.test_common import test_coords, load_answer
 
 def test_delaunay_tessellation():
     # answer calculated from PASSaGE 2 and exported as a binary connection matrix
-    answer = load_answer("delaunay_answer.txt")
+    answer = load_answer("answers/delaunay_answer.txt")
 
     coords = test_coords()
     tessellation, connections = pyssage.distcon.delaunay_tessellation(coords[:, 0], coords[:, 1],
@@ -17,29 +18,75 @@ def test_delaunay_tessellation():
                                    title="Delaunay Connections Test")
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert answer[i, j] == connections[i, j]
+            assert connections[i, j] == answer[i, j]
 
 
 def test_euc_dist_matrix():
     # answer calculated from PASSaGE 2 and exported to 5 decimals
-    answer = load_answer("euc_distmat_answer.txt")
+    answer = load_answer("answers/euc_distmat_answer.txt")
 
     coords = test_coords()
     output = pyssage.distcon.euc_dist_matrix(coords[:, 0], coords[:, 1])
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert answer[i, j] == round(output[i, j], 5)
+            assert round(output[i, j], 5) == answer[i, j]
+
+
+def test_euc_angle_matrix():
+    x = [0, 0, 0, 45, 45, 45, -45, -45, -45]
+    y = [0, 45, -45, 0, 45, -45, 0, 45, -45]
+    output = pyssage.distcon.euc_angle_matrix(numpy.array(x), numpy.array(y))
+    for i in range(len(output)):
+        for j in range(i):
+            if i != j:
+                print(x[i], y[i], x[j], y[j], output[i, j], output[i, j] * 180 / math.pi)
+    print()
+    x = [0, 0, 0, 45, 45, 45, -45, -45, -45]
+    y = [0, 45, -45, 0, 45, -45, 0, 45, -45]
+    output = pyssage.distcon.euc_angle_matrix(numpy.array(x), numpy.array(y), do360=True)
+    for i in range(len(output)):
+        for j in range(len(output)):
+            print(x[i], y[i], x[j], y[j], output[i, j], output[i, j] * 180 / math.pi)
 
 
 def test_sph_dist_matrix():
-    # answer calculated from PASSaGE 2 and exported to 5 decimals
-    answer = load_answer("sph_distmat_answer.txt")
-
     coords = test_coords()
-    output = pyssage.distcon.sph_dist_matrix(coords[:, 0], coords[:, 1])
-    for i in range(len(answer)):
-        for j in range(len(answer)):
-            assert answer[i, j] == round(output[i, j], 5)
+    pyssage.distcon.sph_dist_matrix(coords[:, 0], coords[:, 1])
+    """
+    The original code matched the PASSaGE output, but I decided to go away from predetermined constants for
+    conversion of angels to radians, meaning the numbers shift a bit due to greater floating-point accuracy. I am
+    also be using a better estimate of the radius of the Earth in the calculations
+    
+    this change might cascade to other tests that relied upon these distances
+    """
+
+
+def test_sph_angle_matrix():
+    # answer calculated from PASSaGE 2 and exported to 5 decimals
+    # answer matrix is in degrees, so need to convert output to degrees to match
+    # answer = load_answer("answers/sph_anglemat_answer.txt")
+    #
+    # coords = test_coords()
+    # output = pyssage.distcon.sph_angle_matrix(coords[:, 0], coords[:, 1])
+    # for i in range(len(answer)):
+    #     for j in range(len(answer)):
+    #         assert round(output[i, j]*180/math.pi, 5) == answer[i, j]
+
+    # lons = [0, 0, 0, 45, 45, 45, -45, -45, -45]
+    # lats = [0, 45, -45, 0, 45, -45, 0, 45, -45]
+    # output = pyssage.distcon.euc_angle_matrix(numpy.array(lons), numpy.array(lats))
+    # for i in range(len(output)):
+    #     for j in range(len(output)):
+    #         if i != j:
+    #             print(lons[i], lats[i], lons[j], lats[j], output[i, j], output[i, j]*180/math.pi)
+
+    lons = [0, 0, 0, 45, 45, 45, -45, -45, -45]
+    lats = [0, 45, -45, 0, 45, -45, 0, 45, -45]
+    output = pyssage.distcon.sph_angle_matrix(numpy.array(lons), numpy.array(lats))
+    for i in range(len(output)):
+        for j in range(len(output)):
+            if i != j:
+                print(lons[i], lats[i], lons[j], lats[j], output[i, j], output[i, j] * 180 / math.pi)
 
 
 def test_check_input_distance_matrix():
@@ -94,7 +141,7 @@ def test_setup_connection_output():
 
 def test_relative_neighborhood_network():
     # answer calculated from PASSaGE 2 and exported as a binary connection matrix
-    answer = load_answer("rel_neighbor_answer.txt")
+    answer = load_answer("answers/rel_neighbor_answer.txt")
 
     coords = test_coords()
     distances = pyssage.distcon.sph_dist_matrix(coords[:, 0], coords[:, 1])
@@ -103,12 +150,12 @@ def test_relative_neighborhood_network():
                                    title="Relative Neighborhood Network Test")
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert answer[i, j] == connections[i, j]
+            assert connections[i, j] == answer[i, j]
 
 
 def test_gabriel_network():
     # answer calculated from PASSaGE 2 and exported as a binary connection matrix
-    answer = load_answer("gabriel_answer.txt")
+    answer = load_answer("answers/gabriel_answer.txt")
 
     coords = test_coords()
     distances = pyssage.distcon.sph_dist_matrix(coords[:, 0], coords[:, 1])
@@ -117,12 +164,12 @@ def test_gabriel_network():
                                    title="Gabriel Graph/Network Test")
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert answer[i, j] == connections[i, j]
+            assert connections[i, j] == answer[i, j]
 
 
 def test_minimum_spanning_tree():
     # answer calculated from PASSaGE 2 and exported as a binary connection matrix
-    answer = load_answer("minspan_answer.txt")
+    answer = load_answer("answers/minspan_answer.txt")
 
     coords = test_coords()
     distances = pyssage.distcon.sph_dist_matrix(coords[:, 0], coords[:, 1])
@@ -131,7 +178,7 @@ def test_minimum_spanning_tree():
                                    title="Minimum-Spanning Tree Test")
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert answer[i, j] == connections[i, j]
+            assert connections[i, j] == answer[i, j]
 
 
 def test_connect_distance_range():
@@ -152,7 +199,7 @@ def test_connect_distance_range():
                                    title="Distance-based Connections (25-100) Test")
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert answer[i, j] == connections[i, j]
+            assert connections[i, j] == answer[i, j]
 
     answer = load_answer("distance_based_connect_50-150_answer.txt")
     connections = pyssage.distcon.connect_distance_range(distances, mindist=50, maxdist=150, output_frmt="binmatrix")
@@ -160,7 +207,7 @@ def test_connect_distance_range():
                                    title="Distance-based Connections (50-150) Test")
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert answer[i, j] == connections[i, j]
+            assert connections[i, j] == answer[i, j]
 
 
 def test_least_diagonal_network():
@@ -173,7 +220,7 @@ def test_least_diagonal_network():
     """
     if tested versus PASSaGE the test fails. I believe the PASSaGE code for this algorithm might have been 
     slightly buggy. it could also be an issue of ties being sorted into a different order, but affecting the 
-    outcome. for now I'm believing the 
+    outcome. for now I'm believing the code here is likely correct
     """
     # # answer calculated from PASSaGE 2 and exported as a binary connection matrix
     # # PASSaGE output produced 1's or emtpy cells rather than 1's and 0's, so modification necessary prior to import
@@ -187,7 +234,7 @@ def test_least_diagonal_network():
     #                                title="Least Diagonal Network Test")
     # for i in range(len(answer)):
     #     for j in range(len(answer)):
-    #         assert answer[i, j] == connections[i, j]
+    #         assert connections[i, j] == answer[i, j]
 
 
 def test_nearest_neighbor_connections():
@@ -195,8 +242,8 @@ def test_nearest_neighbor_connections():
     The answer to this did not match that in PASSaGE 2. I have found an error in the old PASSaGE 2 code leading
     to the discrepancy. At this point I believe this code is correct, but do not have a formal test yet.
 
-    For reference, the error in the PASSaGE code was inadvertently resetting of positive connections to
-    negative when looking at nearest neighbors for later points in the list, thus eliminating some of the potential
+    For reference, the error in the PASSaGE code was the inadvertent resetting of positive connections to
+    negative when looking at nearest neighbors for later points in the list, thus eliminating some potential
     asymmetric connections
     """
     coords = test_coords()
@@ -212,7 +259,7 @@ def test_nearest_neighbor_connections():
 
 def test_shortest_path_distances():
     # answer calculated from PASSaGE 2 and exported to 5 decimals
-    answer = load_answer("shortest_path_minspan_answer.txt")
+    answer = load_answer("answers/shortest_path_minspan_answer.txt")
 
     coords = test_coords()
     distances = pyssage.distcon.sph_dist_matrix(coords[:, 0], coords[:, 1])
@@ -224,7 +271,7 @@ def test_shortest_path_distances():
                                      connection_frmt="boolmatrix")
     for i in range(len(answer)):
         for j in range(len(answer)):
-            assert round(answer[i, j], 0) == round(geodists[i, j], 0)
+            assert round(geodists[i, j], 0) == round(answer[i, j], 0)
             # rounding differences are adding up; at least one of the estimated distances is marginally different at
             # even 1 decimal place (just a single digit) (others are different by one digit at 2, 3, 4, or 5 decimals)
 
